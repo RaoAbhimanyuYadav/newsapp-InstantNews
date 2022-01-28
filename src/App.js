f4bba558fce19374ab2811ae67d8a41b1eb98b45
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-01-28 16:03:29 +0530

    Added Navbar and creates news.js news-item.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 
 import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
-  render() {
-    let name = 'Rao'; 
+  render() { 
     return(
       <div>
-        Hello {name}
+        <Navbar/>
+        <News/>
       </div>
     );  
   }
