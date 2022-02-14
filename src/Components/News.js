db159d5a43390cf486bd352800083533aaceca58
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-02-14 22:53:14 +0530

    done video 34

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,66 +1,63 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
-
-
+import PropTypes from 'prop-types';
 
 export class News extends Component {
-  
-  constructor(){
-    super();
+  static defaultProps = {
+    country : 'in',
+    pageSize: 12,
+    category: 'general'
+  }
+   
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize : PropTypes.number,
+    category: PropTypes.string,
+  }
+  constructor(props){
+    super(props);
     this.state={
       articles: [],
       loading : false,
       page:1,
       totalResults:0
     }
+    document.title  = `${this.props.category.toUpperCase().slice(0,1)+this.props.category.slice(1)}-- Instant News`
 
   }
-  //async can wait for its body component to resolve promises
-  async componentDidMount(){// firstly constuctor run then render then compnentDidMOunt
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=7e82a05404e5430eac9b21510d5cd693&pageSize=${this.props.pageSize}&spage=1`;
+  async updateNews(){
+    const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7e82a05404e5430eac9b21510d5cd693&pageSize=${this.props.pageSize}&page=${this.state.page}`;
     this.setState({loading:true})
     let data=await fetch(url); //fetch api and return a promise
     let parsedData =await data.json();
     this.setState ({articles : parsedData.articles,totalResults:parsedData.totalResults,
       loading: false})
+  }
+  //async can wait for its body component to resolve promises
+  async componentDidMount(){// firstly constuctor run then render then compnentDidMOunt
+    this.updateNews();
 
   }
   handleNextClick= async()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=7e82a05404e5430eac9b21510d5cd693&pageSize=${this.props.pageSize}&page=${this.state.page + 1}`;
-    this.setState({loading:true})
-    let data=await fetch(url); //fetch api and return a promise
-    let parsedData =await data.json();
-    this.setState({
-      page: this.state.page + 1,
-      articles : parsedData.articles,
-      loading: false
-    })  
-
-
+    this.setState({page:this.state.page+1})
+    this.updateNews()
   }
   handlePreviousClick= async()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=7e82a05404e5430eac9b21510d5cd693&pageSize=${this.props.pageSize}&page=${this.state.page - 1}`;
-    this.setState({loading:true})
-    let data=await fetch(url); //fetch api and return a promise
-    let parsedData =await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles : parsedData.articles,
-      loading: false
-    })
-
+    this.setState({page:this.state.page-1})
+    this.updateNews()
   }
+
   render() {
     return(
         <div className='container my-3'>
 
-          <h2>Instant News -- Top headlines</h2>  
+          <h1 className='container text-center'>Instant News -- Top {this.props.category.toUpperCase().slice(0,1)+this.props.category.slice(1) } headlines</h1>  
           {this.state.loading && <Spinner/>}
             <div className="row">
               {!this.state.loading && this.state.articles.map((element)=>{
                 return <div key ={element.url} className="col-md-4">
-                <NewsItem title={element.title} discription={element.content?element.content.slice(0,100):""} imgUrl={element.urlToImage} newsUrl={element.url}/>
+                <NewsItem title={element.title} discription={element.content?element.content.slice(0,100):""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                 </div>
               })}
             </div>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
-import logo from './unnamed.jpg'
+import React, { Component } from "react";
+import logo from "./icon.jpg";
 
 export class NewsItem extends Component {
   render() {
-    let {title, discription, imgUrl, newsUrl} = this.props;
+    let { title, discription, imgUrl, newsUrl, author, date , source} = this.props;
     return (
-      <div className="card my-3" style={{width: "18rem"}}>
-        <img src={imgUrl?imgUrl:logo} className="card-img-top" alt="..."/>
+      <div className="card my-3">
+        <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}> {source}
+        </span>
+        <img src={imgUrl ? imgUrl : logo} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{discription}...</p>
-          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+            Read more
+          </a>
         </div>
       </div>
     );
   }
 }
- 
+
 export default NewsItem;
